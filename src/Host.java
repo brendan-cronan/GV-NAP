ab12ad4bf9875c862ab401058888bd4f07824d37
Brendan Cronan <32400374+brendan-cronan@users.noreply.github.com>
2017-11-01 11:36:26 -0400

    working on GUI

--- src/Host.java
+++ src/Host.java
@@ -1,8 +1,32 @@
-class Host{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class Host extends JPanel{
 
 
+  private static final Dimension windowSize=new Dimension(500, 700);
 
 
+  private JPanel ConnectPane;
+  private JPanel FilePane;
+  private JPanel CmdPane;
+
+
+  public Host(){
+    this.setLayout(new BorderLayout());
+    this.setPreferredSize(windowSize);
+    //setBorder(BorderFactory.createCompoundBorder(
+    //                    BorderFactory.createTitledBorder(""),
+    //                    BorderFactory.createEmptyBorder(5,5,5,5)));
+    ConnectPane=new JPanel(new BorderLayout());
+    FilePane=new JPanel(new BorderLayout());
+    CmdPane=new JPanel(new BorderLayout());
+    this.add(ConnectPane);
+    this.add(FilePane);
+    this.add(CmdPane);
+
+  }
 
 
 
@@ -19,6 +43,37 @@ class Host{
 
 
   public static void main(String[] args){
+    JFrame f= new JFrame("GV-NAPSTER PROGRAM");
+    Host h= new Host();
+
+    //centers the frame.
+    Box box = new Box(BoxLayout.Y_AXIS);
+		box.add(Box.createVerticalGlue());
+		box.add(h);
+		box.add(Box.createVerticalGlue());
+		f.getContentPane().add(box);
+    //sets up Jframe
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.pack();
+    f.setLocationRelativeTo(null);
+    f.setVisible(true);
+
+
+  }
+
+  private class ClientListner implements ActionListener {
+
+		    //@Override
+		    public void actionPerformed(ActionEvent e) {
+
+        }
+
+
+
+
+
+
+
 
   }
 }
