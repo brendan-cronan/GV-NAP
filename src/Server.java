2a4f744576070816c4a54b889d10400aefd2edd6
Brendan <Brendan@35.40.126.228>
2017-11-06 16:29:04 -0500

    Finished clientHandler constructor and made ConnectionTypes, dent, ward, schindler

--- src/Server.java
+++ src/Server.java
@@ -32,14 +32,45 @@ class Server{
   }
   
 class ClientHandler extends Thread {
+
+	public static final int PORT = 6603;
 	Socket clientSocket;
-	String username, speed, hostname;
+	String username, hostname;
+	Client client;
 	public ClientHandler(Socket socket) {
 		clientSocket = socket;
-		String[] clientData; 
+		String[] clientData = new String[0];
+		String[] clientFileData = new String[0];
+		String connectionType;
+		ConnectionType connection;
 		
 		try {
-			clientData = Net_Util.recStrArr(clientSocket);
+			while(clientData.length == 0) {
+				clientData = Net_Util.recStrArr(clientSocket);
+			}
+			username = clientData[0];
+			connectionType = clientData[1];
+			if(connectionType.equalsIgnoreCase("ethernet")) {
+				connection = ConnectionType.Ethernet;
+			}else if(connectionType.equalsIgnoreCase("Modem")) {
+				connection = ConnectionType.Modem;
+			}else if(connectionType.equalsIgnoreCase("T1")) {
+				connection = ConnectionType.T1;
+			}else  {
+				connection = ConnectionType.T3;
+			}
+			hostname = clientData[2];
+			client = new Client(clientSocket.getInetAddress(), PORT, username, connection);
+			Net_Util.send(clientSocket, "Client ID Recieved");
+			while(clientFileData.length == 0) {
+				clientFileData = Net_Util.recStrArr(clientSocket);
+			}
+			ArrayList<NapFile> files = new ArrayList<NapFile>();
+			for(int i = 0; i < clientFileData.length; i = i+2) {
+				NapFile x = new NapFile(clientFileData[i], clientFileData[i+1]);
+				files.add(x);
+			}
+			registerClient(client, files);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
