0aeffbebf598fe071697a47e4e5d2fb0b66c0caa
Brendan <Brendan@35.40.126.228>
2017-11-06 15:56:17 -0500

    Server stuff -dent

--- src/Server.java
+++ src/Server.java
@@ -1,14 +1,50 @@
 import java.util.*;
+import java.io.*;
+import java.net.*;
 
 class Server{
-
-  HashMap<Client,ArrayList<NapFile>> fileMap =new HashMap<Client,ArrayList<NapFile>>(100);
-  HashMap<NapFile,ArrayList<Client>> clientMap =new HashMap<NapFile,ArrayList<Client>>(100);
-  Server(){
-
+	public static final int PORT = 6603;
+  private HashMap<Client,ArrayList<NapFile>> fileMap =new HashMap<Client,ArrayList<NapFile>>(100);
+  private HashMap<NapFile,ArrayList<Client>> clientMap =new HashMap<NapFile,ArrayList<Client>>(100);
+  
+  Server(int port){
+	  
+	  while(true) {
+	  Socket client = Net_Util.welcomeClient(port);
+	  
+	  
+	  ClientHandler handler =  new ClientHandler(client);
+	  System.out.println("Connection Established.");
+	  handler.start();
+	  }
+  }
+  public void registerClient(Client client, ArrayList<NapFile> files) {
+		fileMap.put(client, files);
+		for(NapFile file : files) {
+			if(!clientMap.containsKey(file)) {
+				ArrayList<Client> clients= new ArrayList<Client>();
+				clients.add(client);
+				clientMap.put(file, clients);
+			}else {
+				clientMap.get(file).add(client);
+			}
+	  }
   }
 
-
+class ClientHandler extends Thread {
+	Socket clientSocket;
+	String username, speed, hostname;
+	public ClientHandler(Socket socket) {
+		clientSocket = socket;
+		String[] clientData; 
+		try {
+			clientData = Net_Util.recStrArr(clientSocket);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
 
 
 
